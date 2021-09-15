5232558b7f7d22a4172e639368e793173e993b0e
Kevin Bonham <kbonham@wellesley.edu>
2021-09-15 19:56:20 -0400

    getting started

--- src/Hapi.jl
+++ src/Hapi.jl
@@ -1,5 +1,16 @@
 module Hapi
 
-# Write your package code here.
+export build_regex
+
+using FilePaths
+using Dagger
+using ClusterManagers
+
+function build_regex(pattern, groups)
+    for (g, p) in pairs(groups)
+        pattern = replace(pattern, string('{',g,'}')=> "(?<$g>$p)")
+    end
+    return Regex(pattern)
+end
 
 end
