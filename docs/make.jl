1416491682f5bf7baa51352f2d91e5fa09500138
Kevin Bonham, PhD <kevbonham@gmail.com>
2021-12-21 11:50:52 -0500

    update docs build

--- docs/make.jl
+++ docs/make.jl
@@ -6,8 +6,10 @@ makedocs(;
     authors="Kevin Bonham <kevbonham@gmail.com>",
     repo="https://github.com/kescobo/Hapi.jl/blob/{commit}{path}#L{line}",
     sitename="Hapi.jl",
+    strict=:doctest,
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
+        edit_link="main",
         canonical="https://kescobo.github.io/Hapi.jl",
         assets=String[],
     ),
@@ -18,4 +20,6 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/kescobo/Hapi.jl",
+    push_preview=true,
+    devbranch="main"
 )
